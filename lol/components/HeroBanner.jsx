d436530ebeb5b68fc29a208ba741681aab211ec9
pharmluka <99403991+pharmluka@users.noreply.github.com>
2022-06-25 00:39:32 -0700

    sinizibe fitness app
    sinizibe fitness app

---
+++ lol/components/HeroBanner.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Box, Stack, Typography, Button} from '@mui/material';
+import bannerImg from '../assets/images/banner.png';
+const HeroBanner = () => {
+  return (
+    <Box sx={{
+       mt: {
+        lg: '212px',
+        xs: '70px'
+       },
+       ml:{
+        sm: '50px'
+       }
+
+    }} position='relative' p='20px'>
+      <Typography color='#FF2625' fontWeight='600'
+      fontSize='26px'
+      >
+        SINIZIBE
+      </Typography>
+      <Typography fontWeight={700} 
+      sx={{fontSize:{lg: '44px', xs: '40px'}}}
+      mb='23px' mt='30px'
+      >
+        Loose Weight,  <br/>  Sweat and Smile
+      </Typography>
+      <Typography fontSize='22px' lineHeight='35px' mb={4}>
+        Check Out our list of exercises
+      </Typography>
+      <Button variant="contained" color='error'
+      sx={{backgroundColor: '#FF2625',padding: '10px'}}
+      href='#exercises'
+      >
+        Explore Exercises
+      </Button>
+      <Typography
+      fontWeight={600} color="#ff2625"
+      sx={{opacity:0.1,
+    display: {lg: 'block', xs: 'none'}
+    }}
+    fontSize='200px'
+      >Exercises</Typography>
+      <img src={bannerImg} alt='banner' className='hero-banner-img'/>
+    
+    
+    </Box>
+  )
+}
+
+export default HeroBanner
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo.png';
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <Typography>
+        Your Fitness Guide
+      </Typography>
+    </Stack>
+
+  </Box>
+);
+
+export default Footer;
